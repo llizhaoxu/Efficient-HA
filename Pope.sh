cb853978b7ba0430ae5284269e4236b677387fb6
LIZHAOXU <llizhaoxu@icloud.com>
2025-12-13 07:11:46 +0000

    update

--- Pope.sh
+++ Pope.sh
@@ -16,77 +16,77 @@ source activate
 conda activate EHA  
 
 
-python generate_response_pope.py \
-    --device "cuda:0" \
-    --model_id "llava-hf/llava-1.5-7b-hf" \
-    --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/llava-1.5-7b" \
-    --pope_type "random"\
-    --method "greedy" \
-    --max_tokens 10 \
+# python generate_response_pope.py \
+#     --device "cuda:0" \
+#     --model_id "llava-hf/llava-1.5-7b-hf" \
+#     --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/llava-1.5-7b" \
+#     --pope_type "random"\
+#     --method "greedy" \
+#     --max_tokens 10 \
 
-python generate_response_pope.py \
-    --device "cuda:0" \
-    --model_id "llava-hf/llava-1.5-7b-hf" \
-    --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/llava-1.5-7b" \
-    --pope_type "popular"\
-    --method "greedy" \
-    --max_tokens 10 \
+# python generate_response_pope.py \
+#     --device "cuda:0" \
+#     --model_id "llava-hf/llava-1.5-7b-hf" \
+#     --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/llava-1.5-7b" \
+#     --pope_type "popular"\
+#     --method "greedy" \
+#     --max_tokens 10 \
 
-python generate_response_pope.py \
-    --device "cuda:0" \
-    --model_id "llava-hf/llava-1.5-7b-hf" \
-    --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/llava-1.5-7b" \
-    --pope_type "adversarial"\
-    --method "greedy" \
-    --max_tokens 10 \
+# python generate_response_pope.py \
+#     --device "cuda:0" \
+#     --model_id "llava-hf/llava-1.5-7b-hf" \
+#     --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/llava-1.5-7b" \
+#     --pope_type "adversarial"\
+#     --method "greedy" \
+#     --max_tokens 10 \
 
-python generate_response_pope.py \
-    --device "cuda:0" \
-    --model_id "llava-hf/llava-1.5-7b-hf" \
-    --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/llava-1.5-7b" \
-    --pope_type "random"\
-    --method "beam" \
-    --max_tokens 10 \
+# python generate_response_pope.py \
+#     --device "cuda:0" \
+#     --model_id "llava-hf/llava-1.5-7b-hf" \
+#     --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/llava-1.5-7b" \
+#     --pope_type "random"\
+#     --method "beam" \
+#     --max_tokens 10 \
 
-python generate_response_pope.py \
-    --device "cuda:0" \
-    --model_id "llava-hf/llava-1.5-7b-hf" \
-    --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/llava-1.5-7b" \
-    --pope_type "popular"\
-    --method "beam" \
-    --max_tokens 10 \
+# python generate_response_pope.py \
+#     --device "cuda:0" \
+#     --model_id "llava-hf/llava-1.5-7b-hf" \
+#     --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/llava-1.5-7b" \
+#     --pope_type "popular"\
+#     --method "beam" \
+#     --max_tokens 10 \
 
-python generate_response_pope.py \
-    --device "cuda:0" \
-    --model_id "llava-hf/llava-1.5-7b-hf" \
-    --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/llava-1.5-7b" \
-    --pope_type "adversarial"\
-    --method "beam" \
-    --max_tokens 10 \
+# python generate_response_pope.py \
+#     --device "cuda:0" \
+#     --model_id "llava-hf/llava-1.5-7b-hf" \
+#     --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/llava-1.5-7b" \
+#     --pope_type "adversarial"\
+#     --method "beam" \
+#     --max_tokens 10 \
 
-python generate_response_pope.py \
-    --device "cuda:0" \
-    --model_id "llava-hf/llava-1.5-7b-hf" \
-    --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/llava-1.5-7b" \
-    --pope_type "random"\
-    --method "dola" \
-    --max_tokens 10 \
+# python generate_response_pope.py \
+#     --device "cuda:0" \
+#     --model_id "llava-hf/llava-1.5-7b-hf" \
+#     --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/llava-1.5-7b" \
+#     --pope_type "random"\
+#     --method "dola" \
+#     --max_tokens 10 \
 
-python generate_response_pope.py \
-    --device "cuda:0" \
-    --model_id "llava-hf/llava-1.5-7b-hf" \
-    --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/llava-1.5-7b" \
-    --pope_type "popular"\
-    --method "dola" \
-    --max_tokens 10 \
+# python generate_response_pope.py \
+#     --device "cuda:0" \
+#     --model_id "llava-hf/llava-1.5-7b-hf" \
+#     --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/llava-1.5-7b" \
+#     --pope_type "popular"\
+#     --method "dola" \
+#     --max_tokens 10 \
 
-python generate_response_pope.py \
-    --device "cuda:0" \
-    --model_id "llava-hf/llava-1.5-7b-hf" \
-    --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/llava-1.5-7b" \
-    --pope_type "adversarial"\
-    --method "dola" \
-    --max_tokens 10 \
+# python generate_response_pope.py \
+#     --device "cuda:0" \
+#     --model_id "llava-hf/llava-1.5-7b-hf" \
+#     --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/llava-1.5-7b" \
+#     --pope_type "adversarial"\
+#     --method "dola" \
+#     --max_tokens 10 \
 
 
 python generate_response_pope.py \
--- Pope2.sh
+++ Pope2.sh
@@ -16,77 +16,77 @@ source activate
 conda activate EHA  
 
 
-python generate_response_pope.py \
-    --device "cuda:0" \
-    --model_id "OpenGVLab/InternVL3-8B" \
-    --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/InternVL3-8B" \
-    --pope_type "random"\
-    --method "greedy" \
-    --max_tokens 10 \
+# python generate_response_pope.py \
+#     --device "cuda:0" \
+#     --model_id "OpenGVLab/InternVL3-8B" \
+#     --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/InternVL3-8B" \
+#     --pope_type "random"\
+#     --method "greedy" \
+#     --max_tokens 10 \
 
-python generate_response_pope.py \
-    --device "cuda:0" \
-    --model_id "OpenGVLab/InternVL3-8B" \
-    --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/InternVL3-8B" \
-    --pope_type "popular"\
-    --method "greedy" \
-    --max_tokens 10 \
+# python generate_response_pope.py \
+#     --device "cuda:0" \
+#     --model_id "OpenGVLab/InternVL3-8B" \
+#     --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/InternVL3-8B" \
+#     --pope_type "popular"\
+#     --method "greedy" \
+#     --max_tokens 10 \
 
-python generate_response_pope.py \
-    --device "cuda:0" \
-    --model_id "OpenGVLab/InternVL3-8B" \
-    --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/InternVL3-8B" \
-    --pope_type "adversarial"\
-    --method "greedy" \
-    --max_tokens 10 \
+# python generate_response_pope.py \
+#     --device "cuda:0" \
+#     --model_id "OpenGVLab/InternVL3-8B" \
+#     --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/InternVL3-8B" \
+#     --pope_type "adversarial"\
+#     --method "greedy" \
+#     --max_tokens 10 \
 
-python generate_response_pope.py \
-    --device "cuda:0" \
-    --model_id "OpenGVLab/InternVL3-8B" \
-    --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/InternVL3-8B" \
-    --pope_type "random"\
-    --method "beam" \
-    --max_tokens 10 \
+# python generate_response_pope.py \
+#     --device "cuda:0" \
+#     --model_id "OpenGVLab/InternVL3-8B" \
+#     --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/InternVL3-8B" \
+#     --pope_type "random"\
+#     --method "beam" \
+#     --max_tokens 10 \
 
-python generate_response_pope.py \
-    --device "cuda:0" \
-    --model_id "OpenGVLab/InternVL3-8B" \
-    --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/InternVL3-8B" \
-    --pope_type "popular"\
-    --method "beam" \
-    --max_tokens 10 \
+# python generate_response_pope.py \
+#     --device "cuda:0" \
+#     --model_id "OpenGVLab/InternVL3-8B" \
+#     --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/InternVL3-8B" \
+#     --pope_type "popular"\
+#     --method "beam" \
+#     --max_tokens 10 \
 
-python generate_response_pope.py \
-    --device "cuda:0" \
-    --model_id "OpenGVLab/InternVL3-8B" \
-    --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/InternVL3-8B" \
-    --pope_type "adversarial"\
-    --method "beam" \
-    --max_tokens 10 \
+# python generate_response_pope.py \
+#     --device "cuda:0" \
+#     --model_id "OpenGVLab/InternVL3-8B" \
+#     --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/InternVL3-8B" \
+#     --pope_type "adversarial"\
+#     --method "beam" \
+#     --max_tokens 10 \
 
-python generate_response_pope.py \
-    --device "cuda:0" \
-    --model_id "OpenGVLab/InternVL3-8B" \
-    --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/InternVL3-8B" \
-    --pope_type "random"\
-    --method "dola" \
-    --max_tokens 10 \
+# python generate_response_pope.py \
+#     --device "cuda:0" \
+#     --model_id "OpenGVLab/InternVL3-8B" \
+#     --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/InternVL3-8B" \
+#     --pope_type "random"\
+#     --method "dola" \
+#     --max_tokens 10 \
 
-python generate_response_pope.py \
-    --device "cuda:0" \
-    --model_id "OpenGVLab/InternVL3-8B" \
-    --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/InternVL3-8B" \
-    --pope_type "popular"\
-    --method "dola" \
-    --max_tokens 10 \
+# python generate_response_pope.py \
+#     --device "cuda:0" \
+#     --model_id "OpenGVLab/InternVL3-8B" \
+#     --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/InternVL3-8B" \
+#     --pope_type "popular"\
+#     --method "dola" \
+#     --max_tokens 10 \
 
-python generate_response_pope.py \
-    --device "cuda:0" \
-    --model_id "OpenGVLab/InternVL3-8B" \
-    --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/InternVL3-8B" \
-    --pope_type "adversarial"\
-    --method "dola" \
-    --max_tokens 10 \
+# python generate_response_pope.py \
+#     --device "cuda:0" \
+#     --model_id "OpenGVLab/InternVL3-8B" \
+#     --output "/projects/_ssd/ZhaoxuCode/Efficient-HA/Result/POPE/InternVL3-8B" \
+#     --pope_type "adversarial"\
+#     --method "dola" \
+#     --max_tokens 10 \
 
 
 python generate_response_pope.py \
